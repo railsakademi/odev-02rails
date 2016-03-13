30f7e6b66ec78060e5e569abc88e7583afa14d9f
Melahat Mindivanli <MelahatMindivanli@users.noreply.github.com>
2016-03-13 14:40:21 +0200

    Added files via upload

---
+++ db/20160313004727_add_movies_to_archive.rb
@@ -0,0 +1,12 @@
+class AddMoviesToArchive < ActiveRecord::Migration
+    def change
+   create_table :movies do |t|
+     t.string :title
+     t.text :description
+     t.string :director
+     t.string :country
+     t.integer :date
+     t.timestamps null: false
+   end
+  end
+end
