3dbdcb6a69b2540fa52101299bff9d0e8fbee892
Victor Tomaili <i@vctor.net>
2016-03-16 01:53:02 +0200

    try to pgsql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160315213343) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "movies", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
